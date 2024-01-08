94a670d7f3ea828a227021260d27adaf4802b493
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-08 19:04:12 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,11 +1,11 @@
 function solution(my_string, target) {
-    var answer = 0;
     
-    const isNeedle = my_string.includes(target);
-    if(isNeedle){
-        answer = 1;
-    }else{
-        answer = 0;
-    }
-    return answer;
+    
+    // const isNeedle = my_string.includes(target);
+    // if(isNeedle){
+    //     answer = 1;
+    // }else{
+    //     answer = 0;
+    // }
+    return my_string.includes(target) ? 1 : 0 ;
 }
