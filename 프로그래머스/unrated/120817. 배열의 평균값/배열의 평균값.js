74011a3dda9455a5a566507111cb5d19c92fbb59
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-09 17:06:17 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -1,8 +1,8 @@
 function solution(numbers) {
     var answer = 0;
-     for(i = 0; i<numbers.length; i++){
-        answer += numbers[i] ;
-        }
-        answer = answer/numbers.length;
+    for(let i = 0; i<numbers.length; i++){
+        answer += numbers[i]
+    }
+    answer = answer/numbers.length;
     return answer;
 }
