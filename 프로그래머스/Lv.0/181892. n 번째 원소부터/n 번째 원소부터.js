fb88af17d4f7fdac095552722754a8aab31593fb
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-12 15:56:10 +0900

    [level Lv.0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/Lv.0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,8 +1,5 @@
 function solution(num_list, n) {
     var answer = [];
-    for( let i = n-1; i< num_list.length; i++){
-        answer.push(num_list[i]);
-        
-    }
-    return answer;
+    
+    return answer = num_list.slice(n-1);
 }
