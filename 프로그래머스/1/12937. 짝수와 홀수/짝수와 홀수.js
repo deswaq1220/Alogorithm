c3c3ef4ada5be827a09cc29501dfd45ebcd13959
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-18 14:59:04 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,11 @@
 function solution(num) {
     var answer = '';
-    if(num%2 === 0){
-        answer = 'Even'
-    }else{
-        answer ="Odd"
-    }
+    // if(num%2 === 0){
+    //     answer = 'Even'
+    // }else{
+    //     answer ="Odd"
+    // }
+    
+    answer = num%2 === 0 ? 'Even' : 'Odd'
     return answer;
 }
