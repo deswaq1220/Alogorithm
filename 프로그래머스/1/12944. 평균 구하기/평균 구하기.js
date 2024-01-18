dbb531c5abcf569caaeb97f269702b359b49ab9f
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-18 14:26:21 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,6 @@
+function solution(arr) {
+    var answer = 0;
+    let avg = arr.reduce((prev,curr) => prev + curr,0) // reduce 메소드를 사용해서 배열의 모든 값을 합산해준다
+    answer = avg/arr.length // 배열 값 합산한거 나누기 배열 길이 
+    return answer;
+}
