2aa0d2b03ebb22156148a8376a886ffc2000d944
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-25 19:37:30 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    var answer = 0;
+    for(let i = a; i<=b; i++){
+        answer += i
+    }
+    if(a>b){
+        for(let j = b; j<=a; j++){
+            answer +=j
+        }
+    }
+    return answer;
+}
