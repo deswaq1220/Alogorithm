68fb8321b3518422a6d6f8150dff4d6fe4ae2e99
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-01-31 19:30:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.13 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 19:27:9
+2024년 1월 3일 19:30:25
 
 ### 문제 설명
 
--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,13 +1,24 @@
-function solution(phone_number) {
-    var answer = '';
+// function solution(phone_number) {
+//     var answer = '';
     
 
-    if(phone_number.length==4) return phone_number
-    for(let i = 0; i<phone_number.length -4; i++){
-        phone_number = phone_number.replace(phone_number[i], '*')
-        answer = phone_number;
-    }
+//     if(phone_number.length==4) return phone_number
+//     for(let i = 0; i<phone_number.length -4; i++){
+//         phone_number = phone_number.replace(phone_number[i], '*')
+//         answer = phone_number;
+//     }
 
    
+//     return answer;
+// }
+
+function solution(phone_number) {
+    let answer = '';
+    let arr = phone_number.split('');
+
+    for(let i = 0; i < arr.length - 4; i++){
+        arr[i] = '*';
+    }
+    answer = arr.join('');
     return answer;
 }
