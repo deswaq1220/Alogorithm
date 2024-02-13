96eebc286baa210e7d75a183f8e583ad83f7ea5b
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-02-13 09:02:47 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,24 @@
+function solution(s) {
+    var answer = true;
+    let regex = /^[0-9]*$/;
+    
+    if(regex.test(s) && (4 == s.length || s.length == 6)){
+        // 정규표현식.test("문자열") => 문자열이 정규표현식과 매칭이되면 true 아니면 flase를 반환해준다
+        return true;
+    }else{
+        
+        return false;
+    }
+}
+
+    // let num = "0123456789"
+    // if(s.length === 4 || s.length === 6){
+    //     for(let i = 0; i<s.length; i++){
+    //         if(num.includes(s[i])){
+    //              console.log(num)
+    //             answer ;
+    //         }else{
+    //             answer = false
+    //         }
+    //     }
+    // }
