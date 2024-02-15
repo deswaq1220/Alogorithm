9c557c9b30ce58e1015b801447c20555a7324bdc
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-02-15 10:33:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,5 +1,11 @@
 function solution(n) {
     var answer = 0;
+    
+// 1. 입력받은 수를 toString(3)
+// 2. split("")
+// 3. reverse()
+// 4. 배열이므로 join("") -> 참고로 join()은 배열 사이에 쉼표가 들어가게된다.
+// 5. parseInt( , 3)
 
     answer = n.toString(3).split('').reverse().join('')
     
