77fc93f6caded396dd06b8608ad3cda0e8e6688c
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-07-19 10:53:54 +0900

    [Bronze V] Title: 등록, Time: 108 ms, Memory: 13780 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        String id = "deswaq789";
+
+        System.out.println(37);
+        System.out.println(id);
+    }
+}
