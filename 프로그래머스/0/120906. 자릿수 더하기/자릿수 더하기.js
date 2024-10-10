62b9712fd904e77c5af3cb4cea5830f8d1d6618e
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-10-10 16:40:23 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,8 @@
 function solution(n) {
-    var answer = n.toString().split("").map(v => parseInt(v)).reduce((acc, cur) => acc + cur, 0);
+    var answer = n
+    .toString()
+    .split("")
+    .map(v => parseInt(v))
+    .reduce((acc, cur) => acc + cur, 0);
     return answer;
 }
