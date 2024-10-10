1282701f8764623273b871b8983637a79d207cfe
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-10-10 13:34:41 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,5 +1,4 @@
 function solution(numbers) {
-    var answer = numbers.reduce((acc,cur) => acc+cur,0);
-    let value = answer/numbers.length
-    return value;
+    var answer = numbers.reduce((acc,cur) => acc+cur,0)/numbers.length;
+    return answer;
 }
