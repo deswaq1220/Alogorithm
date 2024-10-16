d29ea6ec708b1de41bbd91ee28c7a43a3439f1e7
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-10-16 20:08:49 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    var answer = 0;
-    if(n%7 == 0){
-        answer = n / 7
-    }else{
-        answer = n / 7 +1
-    }
-    return parseInt(answer);
+    // var answer = 0;
+    // if(n%7 == 0){
+    //     answer = n / 7
+    // }else{
+    //     answer = n / 7 +1
+    // }
+    // return parseInt(answer);
+    return Math.ceil(n/7)
 }
